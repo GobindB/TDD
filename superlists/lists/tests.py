c22414850f8ab9f6a8e6adcbd47476b0d36dd7a4
Gobind <gobind@thesinghs.org>
2020-07-30 03:15:59 -0700

    First test passed

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 
-class randTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_failiure(self):
-        self.assertEqual(1+1, 3)
+    # unit tests for page
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)

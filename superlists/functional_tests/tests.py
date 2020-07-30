f345bf68dd926f4a0b496ab1e6c926c4d2c66057
Gobind <gobind@thesinghs.org>
2020-07-30 15:35:55 -0700

    pre link isolation refactor

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,20 +1,31 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
 
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        MAX_WAIT = 10
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     # functional test == end to end test == Acceptance test
     # writing the user story:
@@ -38,22 +49,20 @@ class NewVisitorTest(LiveServerTestCase):
 
         # the user hits enter and now the page updates and lists this as an item on the to-do list
         inputBox.send_keys(Keys.ENTER)
-        time.sleep(2)
 
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             '1: Apply for Neuralink & Google internship')
 
         # the text box persists and asks user to enter another item
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # after user hits enter page reloads again and now shows both items on the list
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             '1: Apply for Neuralink & Google internship')
 
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
 
         # user sees that site has generated a unique URL alongside some explanatory text to show data persistence
